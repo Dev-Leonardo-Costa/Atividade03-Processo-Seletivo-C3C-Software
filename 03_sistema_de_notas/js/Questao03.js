ee53e5552cd73c89d355ad13849ee392e95f272a
Leonardo Costa <devleonardo27@gmail.com>
2022-08-27 23:01:37 -0300

    implementado as atividades restante

--- 03_sistema_de_notas/js/Questao03.js
+++
@@ -1,9 +0,0 @@
-resultado.onclick = function () {
-  let nota = document.querySelector(".nota").value;
-  ladoA = Number(nota);
-
-  let resultado = +(nota.toFixed(2));
-
-  document.querySelector('resultado').innerHTML = resultado;
-  
-};
